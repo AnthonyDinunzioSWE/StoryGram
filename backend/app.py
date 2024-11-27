9707feccbf039857d2686063d8d5f554b56321bc
Anthony Dinunzio <anthonydinunzioswe@gmail.com>
2024-11-26 17:01:08 -0700

    Fixing More Import Errors

--- backend/app.py
+++ backend/app.py
@@ -36,7 +36,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # DATABASE MODELS
-from config import db
 from datetime import datetime
 
 # User Table
